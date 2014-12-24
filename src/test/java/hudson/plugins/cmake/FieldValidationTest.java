c3479258bef689bcd175593cb713b4acb3ea13dc
Jonathan Romero <jonnyro@stellarscience.com>
2014-12-24 14:22:35 -0700

    Adding more unit tests

--- src/test/java/hudson/plugins/cmake/FieldValidationTest.java
+++ src/test/java/hudson/plugins/cmake/FieldValidationTest.java
@@ -4,30 +4,71 @@ package hudson.plugins.cmake;
 import org.jvnet.hudson.test.JenkinsRule;
 import hudson.model.*;
 import org.junit.Test;
+import org.junit.Rule;
 import hudson.plugins.cmake.CmakeBuilder;
 
 
 public class FieldValidationTest {
 
+
   @Test
-  public void checkValidation() throws Exception {
+  public void checkSourceDirGetter() throws Exception {
+    CmakeBuilder c = new CmakeBuilder(
+          "trunk/CMakeModules/3rdparty",
+          "Buildarea/cmake/3rdparty/Debug",
+          "",
+          "Debug",
+          false,
+          false,
+          "Unix Makefiles",
+          "make",
+          "",
+          "",
+          "",
+          "");
 
-  CmakeBuilder c = new CmakeBuilder(
-        "trunk/CMakeModules/3rdparty",
-        "Buildarea/cmake/3rdparty/Debug",
-        "",
-        "Debug",
-        false,
-        false,
-        "Unix Makefiles",
-        "make",
-        "",
-        "",
-        "",
-        "");
 
-    assert(true);
+    assert(c.getSourceDir() == "trunk/CMakeModules/3rdparty");
 
+  }
+  @Test
+  public void checkBuildDirGetter() throws Exception {
+    CmakeBuilder c = new CmakeBuilder(
+          "trunk/CMakeModules/3rdparty",
+          "Buildarea/cmake/3rdparty/Debug",
+          "",
+          "Debug",
+          false,
+          false,
+          "Unix Makefiles",
+          "make",
+          "",
+          "",
+          "",
+          "");
+
+
+    assert( c.getBuildDir() == "Buildarea/cmake/3rdparty/Debug" );
+
+  }
+  @Test
+  public void checkInstallDirGetter() throws Exception {
+    CmakeBuilder c = new CmakeBuilder(
+          "trunk/CMakeModules/3rdparty",
+          "Buildarea/cmake/3rdparty/Debug",
+          "",
+          "Debug",
+          false,
+          false,
+          "Unix Makefiles",
+          "make",
+          "",
+          "",
+          "",
+          "");
+
+
+    assert(c.getInstallDir() == "");
 
   }
 
