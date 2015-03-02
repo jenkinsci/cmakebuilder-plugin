2542acd21fed0ee724ba5f3cc13213cfcca7f55b
André Klitzing <aklitzing@gmail.com>
2015-03-02 23:14:49 +0100

    [JENKINS-27203] Fix handling of environment variables
    Replace longer variables first to avoid replacing a
    partial long name with a short name.
    
    Example:
     - NODE = abc
     - NODE_ID = 123
    
    Result "abc_ID" instead of "123"

--- src/main/java/hudson/plugins/cmake/EnvVarReplacer.java
+++ src/main/java/hudson/plugins/cmake/EnvVarReplacer.java
@@ -1,12 +1,27 @@
 package hudson.plugins.cmake;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+class StringLengthComparator implements Comparator<String>
+{
+	@Override
+	public int compare(String lhs, String rhs)
+	{
+		return rhs.length() - lhs.length();
+	}
+}
 
 public class EnvVarReplacer {
 	
 	public static String replace(String stringContainingEnvVars, Map<String, String> envVars) {
-		Set<String> keys = envVars.keySet();
+		List<String> keys = new ArrayList<String>(envVars.keySet());
+		Collections.sort(keys, new StringLengthComparator());
+
     	for (String key : keys) {
     		stringContainingEnvVars = 
     			stringContainingEnvVars.replaceAll("\\$" + key, envVars.get(key));
