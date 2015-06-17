fd0ab9acd957bfe4739c86744483598c3c73351f
15knots <fifteenknots505@gmail.com>
2015-06-17 21:26:15 +0200

    separated test for JENKINS-27203. added test for std form ${VAR]

--- src/test/java/hudson/plugins/cmake/EnvVarReplacerTest.java
+++ src/test/java/hudson/plugins/cmake/EnvVarReplacerTest.java
@@ -1,6 +1,7 @@
 package hudson.plugins.cmake;
 
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
@@ -11,22 +12,39 @@ public class EnvVarReplacerTest {
 
   @Test
   public void checkReplacement() throws Exception {
-    String strWithEnvVars = "Is $HOME in $PATH? $NODE $NODE_ID";
-    String desired = "Is /home/jonnyro in /usr/bin/;/usr/local/bin/? abc123 987";
+    String strWithEnvVars = "Is $HOME in $PATH?";
+    String desired = "Is /home/jonnyro in /usr/bin/;/usr/local/bin/?";
     HashMap<String,String> envVars = new HashMap<String,String>();
 
-    envVars.put("HOME","/home/jonnyro"); 
+    envVars.put("HOME","/home/jonnyro");
     envVars.put("PATH","/usr/bin/;/usr/local/bin/");
-    envVars.put("NODE","abc123");
-    envVars.put("NODE_ID","987");
 
-    EnvVarReplacer e = new EnvVarReplacer();
+    String res = EnvVarReplacer.replace(strWithEnvVars, envVars);
 
-    String res = e.replace(strWithEnvVars, envVars);
-  
-    assertTrue("'" + res + "'!='" + desired + "'", res.equals(desired) ); 
-
+    assertTrue("'" + res + "'!='" + desired + "'", res.equals(desired) );
   }
 
+  @Test
+  // @Issue("JENKINS-27203")
+  public final void testExpandLengthDependent() {
+    String strWithEnvVars = "Is $HOME in? $NODE $NODE_ID";
+    String expected = "Is $HOME in? abc123 987";
 
+    HashMap<String,String> envVars = new HashMap<String,String>();
+    envVars.put("NODE","abc123");
+    envVars.put("NODE_ID","987");
+    String res = EnvVarReplacer.replace(strWithEnvVars, envVars);
+    assertEquals(expected,res);
+  }
+
+  @Test
+  public final void testBracketedFrorm() {
+    HashMap<String,String> envVars = new HashMap<String,String>();
+    envVars.put("NODE","abc123");
+    envVars.put("NODE_ID","987");
+    // test whether the usual syntax of jenkins is supported
+    String strWithEnvVars = "Is ${HOME} in? ${NODE} ${NODE_ID}";
+    String res = EnvVarReplacer.replace(strWithEnvVars, envVars);
+    assertEquals("Is ${HOME} in? abc123 987",res);
+  }
 }
