6d3bc30938acd10547c5f43a44e7face3e6200ef
15knots <fifteenknots505@gmail.com>
2015-06-18 21:06:55 +0200

    hudson.Util.replaceMacro() does what we need

--- src/main/java/hudson/plugins/cmake/EnvVarReplacer.java
+++ src/main/java/hudson/plugins/cmake/EnvVarReplacer.java
@@ -1,31 +1,10 @@
 package hudson.plugins.cmake;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Map;
 
-class StringLengthComparator implements Comparator<String>
-{
-	@Override
-	public int compare(String lhs, String rhs)
-	{
-		return rhs.length() - lhs.length();
-	}
-}
-
 public class EnvVarReplacer {
-	
+
 	public static String replace(String stringContainingEnvVars, Map<String, String> envVars) {
-		List<String> keys = new ArrayList<String>(envVars.keySet());
-		Collections.sort(keys, new StringLengthComparator());
-
-    	for (String key : keys) {
-    		stringContainingEnvVars = 
-    			stringContainingEnvVars.replaceAll("\\$" + key, envVars.get(key));
-    	}
-    	return stringContainingEnvVars;
+	  return hudson.Util.replaceMacro(stringContainingEnvVars, envVars);
 	}
-
 }
