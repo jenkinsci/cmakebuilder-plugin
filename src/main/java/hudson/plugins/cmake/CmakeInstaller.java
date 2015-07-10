9f4c9bdc20caa964f8ef41955a6d65a98f1a71bc
15knots <fifteenknots505@gmail.com>
2015-07-10 21:34:33 +0200

    formatted

--- src/main/java/hudson/plugins/cmake/CmakeInstaller.java
+++ src/main/java/hudson/plugins/cmake/CmakeInstaller.java
@@ -6,7 +6,12 @@
  *******************************************************************************/
 package hudson.plugins.cmake;
 
+import java.io.IOException;
+
 import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.TaskListener;
+import hudson.model.Node;
 import hudson.tools.DownloadFromUrlInstaller;
 import hudson.tools.ToolInstallation;
 
@@ -16,21 +21,32 @@ import org.kohsuke.stapler.DataBoundConstructor;
  * Automatic Cmake installer from cmake.org.
  */
 public class CmakeInstaller extends DownloadFromUrlInstaller {
-  @DataBoundConstructor
-  public CmakeInstaller(String id) {
-    super(id);
-  }
-
-  @Extension
-  public static final class DescriptorImpl extends
-      DownloadFromUrlInstaller.DescriptorImpl<CmakeInstaller> {
-    public String getDisplayName() {
-      return "Install from cmake.org";
+    @DataBoundConstructor
+    public CmakeInstaller(String id) {
+        super(id);
     }
 
     @Override
-    public boolean isApplicable(Class<? extends ToolInstallation> toolType) {
-      return toolType == CmakeTool.class;
+    public FilePath performInstallation(ToolInstallation tool, Node node,
+            TaskListener log) throws IOException, InterruptedException {
+        // TODO Auto-generated function stub
+        return super.performInstallation(tool, node, log);
     }
-  }
-} // CmakeInstaller
+
+    // //////////////////////////////////////////////////////////////////
+    // inner classes
+    // //////////////////////////////////////////////////////////////////
+
+    @Extension
+    public static final class DescriptorImpl extends
+            DownloadFromUrlInstaller.DescriptorImpl<CmakeInstaller> {
+        public String getDisplayName() {
+            return "Install from cmake.org";
+        }
+
+        @Override
+        public boolean isApplicable(Class<? extends ToolInstallation> toolType) {
+            return toolType == CmakeTool.class;
+        }
+    } // DescriptorImpl
+}
