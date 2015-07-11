c8b55cad6b97ca475b21808030da8a71f7bb28bb
15knots <fifteenknots505@gmail.com>
2015-07-11 15:50:10 +0200

    progress check-in

--- src/main/java/hudson/plugins/cmake/CmakeInstaller.java
+++ src/main/java/hudson/plugins/cmake/CmakeInstaller.java
@@ -6,15 +6,17 @@
  *******************************************************************************/
 package hudson.plugins.cmake;
 
-import java.io.IOException;
-
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.model.TaskListener;
 import hudson.model.Node;
+import hudson.remoting.Callable;
 import hudson.tools.DownloadFromUrlInstaller;
 import hudson.tools.ToolInstallation;
 
+import java.io.IOException;
+
+import org.jenkinsci.remoting.RoleChecker;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
@@ -33,6 +35,24 @@ public class CmakeInstaller extends DownloadFromUrlInstaller {
         return super.performInstallation(tool, node, log);
     }
 
+    /**
+     * Overwritten to fill in the OS-ARCH specific URL.
+     *
+     * @return null if no such ID is found.
+     */
+    public Installable getInstallable() throws IOException {
+        for (Installable i : ((DescriptorImpl) getDescriptor())
+                .getInstallables())
+            if (id.equals(i.id)) {
+                // Gather properties for the node to install on
+                String[] properties = node.getChannel().call(
+                        new GetSystemProperties("os.name", "os.arch",
+                                "os.version"));
+                return i;
+            }
+        return null;
+    }
+
     // //////////////////////////////////////////////////////////////////
     // inner classes
     // //////////////////////////////////////////////////////////////////
@@ -49,4 +69,35 @@ public class CmakeInstaller extends DownloadFromUrlInstaller {
             return toolType == CmakeTool.class;
         }
     } // DescriptorImpl
+
+    /**
+     * A Callable that gets the values of the given Java system properties from
+     * the (remote) node.
+     * */
+    private static class GetSystemProperties implements
+            Callable<String[], InterruptedException> {
+        private static final long serialVersionUID = 1L;
+
+        private final String[] properties;
+
+        GetSystemProperties(String... properties) {
+            this.properties = properties;
+        }
+
+        public String[] call() {
+            String[] values = new String[properties.length];
+            for (int i = 0; i < properties.length; i++) {
+                values[i] = System.getProperty(properties[i]);
+            }
+            return values;
+        }
+
+        /*-
+         * @see org.jenkinsci.remoting.RoleSensitive#checkRoles(org.jenkinsci.remoting.RoleChecker)
+         */
+        @Override
+        public void checkRoles(RoleChecker checker) throws SecurityException {
+            // TODO Auto-generated function stub
+        }
+    } // GetSystemProperties
 }
