ed1f0a305f3eefb0abbb6c62fd5d84811d745ccb
15knots <fifteenknots505@gmail.com>
2015-07-20 19:47:57 +0200

    Download works

--- src/main/java/hudson/plugins/cmake/CmakeInstaller.java
+++ src/main/java/hudson/plugins/cmake/CmakeInstaller.java
@@ -9,12 +9,20 @@ package hudson.plugins.cmake;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.model.TaskListener;
+import hudson.model.DownloadService.Downloadable;
 import hudson.model.Node;
 import hudson.remoting.Callable;
 import hudson.tools.DownloadFromUrlInstaller;
 import hudson.tools.ToolInstallation;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.json.JSONObject;
 
 import org.jenkinsci.remoting.RoleChecker;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -23,6 +31,8 @@ import org.kohsuke.stapler.DataBoundConstructor;
  * Automatic Cmake installer from cmake.org.
  */
 public class CmakeInstaller extends DownloadFromUrlInstaller {
+    private transient String[] nodeProperties;
+
     @DataBoundConstructor
     public CmakeInstaller(String id) {
         super(id);
@@ -32,6 +42,10 @@ public class CmakeInstaller extends DownloadFromUrlInstaller {
     public FilePath performInstallation(ToolInstallation tool, Node node,
             TaskListener log) throws IOException, InterruptedException {
         // TODO Auto-generated function stub
+        // Gather properties for the node to install on
+        this.nodeProperties = node.getChannel().call(
+                new GetSystemProperties("os.name", "os.arch"));
+
         return super.performInstallation(tool, node, log);
     }
 
@@ -41,17 +55,30 @@ public class CmakeInstaller extends DownloadFromUrlInstaller {
      * @return null if no such ID is found.
      */
     public Installable getInstallable() throws IOException {
-        for (Installable i : ((DescriptorImpl) getDescriptor())
-                .getInstallables())
-            if (id.equals(i.id)) {
-                // Gather properties for the node to install on
-                String[] properties = node.getChannel().call(
-                        new GetSystemProperties("os.name", "os.arch",
-                                "os.version"));
-                return i;
-            }
-        return null;
+        try {
+            List<CmakeInstallable> installables = ((DescriptorImpl) getDescriptor())
+                    .getInstallables();
+
+            for (CmakeInstallable inst : installables)
+                if (id.equals(inst.id)) {
+                    // Filter variants to install by system-properties
+                    final String nodeOsName = nodeProperties[0];
+                    final String nodeOsArch = nodeProperties[1];
+                    // for the node to install on
+                    for (CmakeVariant variant : inst.variants) {
+                        if (variant.appliesTo(nodeOsName, nodeOsArch)) {
+                            CmakeInstallable inst2 = new CmakeInstallable(inst,
+                                    variant);
+                            return inst2;
+                        }
+                    }
+                }
+            return null;
+        } finally {
+            nodeProperties = null; // for GC
+        }
     }
+
 
     // //////////////////////////////////////////////////////////////////
     // inner classes
@@ -62,6 +89,25 @@ public class CmakeInstaller extends DownloadFromUrlInstaller {
             DownloadFromUrlInstaller.DescriptorImpl<CmakeInstaller> {
         public String getDisplayName() {
             return "Install from cmake.org";
+        }
+
+        /**
+         * List of installable tools.
+         *
+         * <p>
+         * The UI uses this information to populate the drop-down.
+         *
+         * @return never null.
+         */
+        public List<CmakeInstallable> getInstallables() throws IOException {
+            JSONObject d = Downloadable.get(getId()).getData();
+            if (d == null)
+                return Collections.emptyList();
+            Map<String, Class<?>> classMap = new HashMap<String, Class<?>>();
+            // classMap.put("versions", CmakeVariant.class);
+            classMap.put("variants", CmakeVariant.class);
+            return Arrays.asList(((CmakeInstallableList) JSONObject.toBean(d,
+                    CmakeInstallableList.class, classMap)).list);
         }
 
         @Override
@@ -100,4 +146,96 @@ public class CmakeInstaller extends DownloadFromUrlInstaller {
             // TODO Auto-generated function stub
         }
     } // GetSystemProperties
+
+    // //////////////////////////////////////////////////////////////////
+    // JSON desrialization
+    // //////////////////////////////////////////////////////////////////
+    /**
+     * Represents the de-serialized JSON data file containing all installable
+     * Cmake versions. See file hudson.plugins.cmake.CmakeInstaller
+     */
+    @Extension
+    public static final class CmakeInstallableList {
+        // initialize with an empty array just in case JSON doesn't have the
+        // list field (which shouldn't happen.)
+        // Public for JSON deserialisation
+        public CmakeInstallable[] list = new CmakeInstallable[0];
+    } // CmakeInstallableList
+
+    // Needs to be public for JSON deserialisation
+    public static class CmakeVariant {
+        public String url;
+        // these come frome the JSON file and finally from cmake´s update site
+        // URLs
+        public String os = "";
+        public String arch = "";
+
+        /**
+         * Checks whether an installation of this CmakeVariant will work on the
+         * given node. This checks the given JVM system properties of a node.
+         *
+         * @param nodeOsName
+         *            the value of the JVM system property "os.name" of the node
+         * @param nodeOsArch
+         *            the value of the JVM system property "os.arch" of the node
+         */
+        public boolean appliesTo(String nodeOsName, String nodeOsArch) {
+            if ("Linux".equals(nodeOsName)) {
+                if (os.equals(nodeOsName)) {
+                    if (nodeOsArch.equals("i386") && arch.equals("i386")) {
+                        return true;
+                    }
+                    if (nodeOsArch.equals("amd64")
+                            && (arch.equals("i386") || arch.equals("x86_64"))) {
+                        return true; // allow both i386 and x86_64
+                    }
+                }
+            } else if (nodeOsName.startsWith("Windows")) {
+                nodeOsName = "win32";
+                if (os.equals(nodeOsName)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+         * @return the value of the JVM system property "os.name" of a node
+         */
+        private String osToJvmProp() {
+            if ("Linux".equalsIgnoreCase(os)) {
+                return "Linux";
+            } else if ("win32".equalsIgnoreCase(os)) {
+                return "win32";
+            } else {
+                // no replacement known yet, users should file a change request
+                return os;
+            }
+        }
+    }
+
+    // Needs to be public for JSON deserialisation
+    public static class CmakeInstallable extends Installable {
+        public CmakeVariant[] variants = new CmakeVariant[0];
+
+        /**
+         * Default ctor for JSON desrialization.
+         */
+        public CmakeInstallable() {
+        }
+
+        /**
+         * @param id
+         *            Used internally to uniquely identify the name.
+         * @param name
+         *            This is the human readable name.
+         */
+        public CmakeInstallable(CmakeInstallable installable,
+                CmakeVariant variant) {
+            super.id = installable.id;
+            super.name = installable.name;
+            super.url = variant.url;
+        }
+
+    }
 }
