c390d431112d0bc444dbd9ef87b330ca30495bb3
15knots <fifteenknots505@gmail.com>
2016-05-06 22:36:45 +0200

    opps, find&replaced too much

--- src/test/java/hudson/plugins/cmake/CmakeBuilderBuildTest.java
+++ src/test/java/hudson/plugins/cmake/CmakeBuilderBuildTest.java
@@ -102,7 +102,7 @@ public class CmakeBuilderBuildTest {
         StringParameterDefinition pd3 = new StringParameterDefinition(
                 "BUILDTYPE", "Release");
         StringParameterDefinition pd4 = new StringParameterDefinition(
-                "BUILDGENERATOR");
+                "BUILDGENERATOR", "Unix Makefiles");
         StringParameterDefinition pd5 = new StringParameterDefinition(
                 "BUILDTOOL", "make");
         StringParameterDefinition pd6 = new StringParameterDefinition(
@@ -113,8 +113,7 @@ public class CmakeBuilderBuildTest {
                 pd1, pd2, pd3, pd4, pd5, pd6, pd7);
         p.addProperty(pdp);
 
-        CmakeBuilder cmb = new CmakeBuilder(CmakeTool.DEFAULT,
-                "${BUILDGENERATOR}");
+        CmakeBuilder cmb = new CmakeBuilder(CmakeTool.DEFAULT);
         cmb.setBuildType("${BUILDTYPE}");
         cmb.setCleanBuild(true);
         cmb.setSourceDir("${SOURCEDIR}");
@@ -141,8 +140,7 @@ public class CmakeBuilderBuildTest {
     public void testBuildToolVariableInjected() throws Exception {
         FreeStyleProject p = j.createFreeStyleProject();
         p.setScm(scm);
-        CmakeBuilder cmb = new CmakeBuilder(CmakeTool.DEFAULT,
-                "Unix Makefiles");
+        CmakeBuilder cmb = new CmakeBuilder(CmakeTool.DEFAULT);
         cmb.setCleanBuild(true);
         cmb.setSourceDir("src");
         cmb.setBuildDir("build");
